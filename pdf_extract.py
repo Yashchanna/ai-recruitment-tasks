b998f6582c8f805f4e52e0666c14cce536285428
Yashchanna <yashchanna123@gmail.com>
2026-06-19 12:55:51 +0530

    Delete pdf_extract.py

--- pdf_extract.py
+++
@@ -1,8 +0,0 @@
-﻿from pypdf import PdfReader
-r = PdfReader(r"E:\Syn Assesment\Gen AI Task.pdf")
-text = []
-for p in r.pages:
-    t = p.extract_text() or ""
-    text.append(t)
-open(r"E:\Syn Assesment\Gen AI Task.txt","w",encoding="utf-8").write("\n\n".join(text))
-print('WROTE')
